ff763b9a9af5c0e93448ede971ec361eafd4039d
Ishita Abrol <iabrolgblvz@gmail.com>
2025-06-26 14:43:36 -0400

    update changes to main

--- server/server.js
+++ server/server.js
@@ -1,27 +1,77 @@
 import express from 'express';
 import { promises as fs } from 'fs';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
 
 const app = express();
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Middleware to parse JSON bodies
 const PORT = 3000;
 
 // Endpoint to read and send JSON file content
 app.get('/socks', async (req, res) => {
     try {
-        // Log the entire request object
-        console.log(req);
-
-        // Log specific parts of the request
-        console.log("Headers:", req.headers);
-        console.log("URL:", req.url);
-        console.log("Method:", req.method);
-        console.log("Query parameters:", req.query);
-
-        const data = await fs.readFile('../data/socks.json', 'utf8');
-        const jsonObj = JSON.parse(data);
-        res.json(jsonObj);
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const socks = await collection.find({}).toArray();
+        res.json(socks);
     } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.post('/socks/search', async (req, res) => {
+    try {
+        const { searchTerm } = req.body;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const regex = new RegExp(searchTerm, 'i'); // Create a case-insensitive regular expression
+        const socks = await collection.find({ 'sockDetails.color': regex }).toArray();
+        res.json(socks);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error searching for socks');
+    }
+});
+
+app.delete('/socks/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const result = await collection.deleteOne({ _id: new ObjectId(id) });
+        if (result.deletedCount === 1) {
+            res.status(200).send('Sock deleted successfully');
+        } else {
+            res.status(404).send('Sock not found');
+        }
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error deleting sock');
+    }
+});
+
+app.post('/socks', async (req, res) => {
+    try {
+        const sock  = req.body;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const result = await collection.insertOne(sock);
+        res.status(201).send(`{"_id":"${result.insertedId}"}`);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error adding sock');
     }
 });
 
@@ -42,10 +92,9 @@ app.get('/socks/:color', async (req, res) => {
     }
 });
 
-// Middleware to parse JSON bodies
-app.use(express.json());
 
-app.post('/socks', async (req, res) => {
+
+app.post('/user', async (req, res) => {
     try {
         // Obligatory reference to POST Malone
         console.log("If POST Malone were a sock, he'd be the one with the most colorful pattern.");
@@ -59,7 +108,7 @@ app.post('/socks', async (req, res) => {
         // Respond with the created user information and a 201 Created status
         res.status(201).send({
             status: 'success',
-            location: 'http://localhost:3000/users/', // This URL should point to the newly created user
+            location: 'http://localhost:3000/users/1234', // This URL should point to the newly created user
             message: 'User created successfully.'
         });
     } catch (err) {
@@ -75,7 +124,7 @@ app.delete('/socks/:id', async (req, res) => {
         res.status(200).send('Sock deleted successfully');
     } catch (err) {
         console.error('Error:', err);
-        res.status(500).send('Hmmm, something smells... No socks for you! ☹');
+        res.status(500).send('Hmm, something doesn\'t smell right... Error deleting sock');
     }
 });
 
